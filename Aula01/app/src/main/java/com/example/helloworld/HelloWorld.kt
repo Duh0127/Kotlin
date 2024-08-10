21c48e9f11fb94c0d03bbf71e895d2c29a5e8491
Duh0127 <rodatupmocgamer22@gmail.com>
2024-08-09 22:55:38 -0300

    Aula02 - [09-08-24]

--- Aula01/app/src/main/java/com/example/helloworld/HelloWorld.kt
+++ Aula01/app/src/main/java/com/example/helloworld/HelloWorld.kt
@@ -2,6 +2,8 @@ package com.example.helloworld
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 
 class HelloWorld : Activity() {
@@ -14,6 +16,20 @@ class HelloWorld : Activity() {
 
         setContentView(txt)
 
+        val btn = Button(this)
+
+//        // TODO - Mover o texto para um resource do tipo String
+//        btn.text = getString(R.string.button_text) // getString pega os textos do res.values.strings.xml
+//        setContentView(btn)
+
+        setContentView(R.layout.helloworld_layout) // importando o arquivo de layout do res.layout
+        Log.v("HELLO", "onCreate() executado")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("HELLO", "onDestroy() executado")
     }
 
 }
